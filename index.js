50063454b5a2a16f32b2d46088080910525fc4cb
fry10k <43117861+fry10k@users.noreply.github.com>
2019-04-27 11:14:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,153 +1,172 @@
-﻿module.exports = function DropNotice(mod) {
+module.exports = function DropNotice(mod) {
 	
 	let enabled = true;
 	
-	mod.command.add('物品', () => {
+	mod.command.add('Item', () => {
 		enabled = !enabled;
-		mod.command.message('模块 ' + enabled ? '<font color="#56B4E9">开启</font>' : '<font color="#E69F00">关闭</font>');
+		mod.command.message('Mod ' + enabled ? '<font color="#56B4E9">On</font>' : '<font color="#E69F00">Off</font>');
 	})
 	
 	mod.hook('S_SPAWN_DROPITEM', 6, (event) => {
 		if(!enabled) return;
 		switch (event.item) {
 			case 368:
-				sendMessage('╰綠寶石╯');break;
+				sendMessage('Emerald');break;
 			case 369:
-				sendMessage('╰鑽石╯');break;
+				sendMessage('Diamond');break;
 			
 			case 6700:
-				sendMessage('╰項鏈刻印書-野獸强打III╯');break;
+				sendMessage('Necklace Etching: Pumped III');break;
 			case 6705:
-				sendMessage('╰項鏈刻印書-强力的野獸强打III╯');break;
+				sendMessage('Necklace Etching: Relentless Pumped III');break;
 			case 6701:
-				sendMessage('╰項鏈刻印書-野獸猛擊III╯');break;
+				sendMessage('Necklace Etching: Keen III');break;
 			case 6706:
-				sendMessage('╰項鏈刻印書-强力的野獸猛擊III╯');break;
+				sendMessage('Necklace Etching: Relentless Keen III');break;
 			case 6702:
-				sendMessage('╰項鏈刻印書-野獸快步III╯');break;
+				sendMessage('Necklace Etching: Swift III');break;
 			case 6707:
-				sendMessage('╰項鏈刻印書-强力的野獸快步III╯');break;
+				sendMessage('Necklace Etching: Relentless Swift III');break;
 			
 			case 6780:
-				sendMessage('╰耳環刻印書-野獸强打III╯');break;
+				sendMessage('Earring Etching: Pumped III');break;
 			case 6785:
-				sendMessage('╰耳環刻印書-强力的野獸强打III╯');break;
+				sendMessage('Earring Etching: Relentless Pumped III');break;
 			case 6781:
-				sendMessage('╰耳環刻印書-野獸猛擊III╯');break;
+				sendMessage('Earring Etching: Keen III');break;
 			case 6786:
-				sendMessage('╰耳環刻印書-强力的野獸猛擊III╯');break;
+				sendMessage('Earring Etching: Relentless Keen III');break;
 			case 6782:
-				sendMessage('╰耳環刻印書-野獸快步III╯');break;
+				sendMessage('Earring Etching: Swift III');break;
 			case 6787:
-				sendMessage('╰耳環刻印書-强力的野獸快步III╯');break;
+				sendMessage('Earring Etching: Relentless Swift III');break;
 			
 			case 6940:
-				sendMessage('╰腰帶刻印書-野獸强打III╯');break;
+				sendMessage('Belt Etching: Pumped III');break;
 			case 6945:
-				sendMessage('╰腰帶刻印書-强力的野獸强打III╯');break;
+				sendMessage('Belt Etching: Relentless Pumped III');break;
 			case 6941:
-				sendMessage('╰腰帶刻印書-野獸猛擊III╯');break;
+				sendMessage('Belt Etching: Keen III');break;
 			case 6946:
-				sendMessage('╰腰帶刻印書-强力的野獸猛擊III╯');break;
+				sendMessage('Belt Etching: Relentless Keen III');break;
 			case 6942:
-				sendMessage('╰腰帶刻印書-野獸快步III╯');break;
+				sendMessage('Belt Etching: Swift III');break;
 			case 6947:
-				sendMessage('╰腰帶刻印書-强力的野獸快步III╯');break;
+				sendMessage('Belt Etching: Relentless Swift III');break;
 			
 			case 6860:
-				sendMessage('╰戒指刻印書-野獸强打III╯');break;
+				sendMessage('Ring Etching: Pumped III');break;
 			case 6865:
-				sendMessage('╰戒指刻印書-强力的野獸强打III╯');break;
+				sendMessage('Ring Etching: Relentless Pumped III');break;
 			case 6861:
-				sendMessage('╰戒指刻印書-野獸猛擊III╯');break;
+				sendMessage('Ring Etching: Keen III');break;
 			case 6866:
-				sendMessage('╰戒指刻印書-强力的野獸猛擊III╯');break;
+				sendMessage('Ring Etching: Relentless Keen III');break;
 			case 6862:
-				sendMessage('╰戒指刻印書-野獸快步III╯');break;
+				sendMessage('Ring Etching: Swift III');break;
 			case 6867:
-				sendMessage('╰戒指刻印書-强力的野獸快步III╯');break;
+				sendMessage('Ring Etching: Relentless Swift III');break;
 			
 			case 6958:
-				sendMessage('╰胸針刻印書-野獸强打I╯');break;
+				sendMessage('Brooch Etching: Pumped I');break;
 			case 6961:
-				sendMessage('╰胸針刻印書-野獸強打II╯');break;
+				sendMessage('Brooch Etching: Pumped II');break;
 			
 			case 28600:
-				sendMessage('╰金色碎片圖╯');break;
+				sendMessage('Design: Golden Daric');break;
 			case 28601:
-				sendMessage('╰金色鈑金圖╯');break;
+				sendMessage('Design: Golden Plate');break;
 			case 28602:
-				sendMessage('╰銀色碎片圖╯');break;
+				sendMessage('Design: Silver Siglo');break;
 			case 28603:
-				sendMessage('╰銀色鈑金圖╯');break;
+				sendMessage('Design: Silver Plate');break;
 			case 28607:
-				sendMessage('╰加工圖:鑽石╯');break;
+				sendMessage('Design: Diamond');break;
 			
 			case 28620:
-				sendMessage('╰加工圖：不平衡的和諧╯');break;
+				sendMessage('Design: Harmonious Imbalance');break;
 			
 			case 88631:
-				sendMessage('╰力量初始面具╯');break;
+				sendMessage('Powerful Onset Mask');break;
 			case 88632:
-				sendMessage('╰暴擊初始面具╯');break;
+				sendMessage('Keen Onset Mask');break;
 			case 88633:
-				sendMessage('╰暴威初始面具╯');break;
+				sendMessage('Bitter Onset Mask');break;
 			case 88634:
-				sendMessage('╰CD初始面具╯');break;
+				sendMessage('Energetic Onset Mask');break;
 			case 88635:
-				sendMessage('╰力量无限面具╯');break;
+				sendMessage('Powerful Infinity Mask');break;
 			case 88636:
-				sendMessage('╰暴擊无限面具╯');break;
+				sendMessage('Keen Infinity Mask');break;
 			case 88637:
-				sendMessage('╰暴威无限面具╯');break;
+				sendMessage('Bitter Infinity Mask');break;
 			case 88638:
-				sendMessage('╰CD无限面具╯');break;
+				sendMessage('Energetic Infinity Mask');break;
 			case 88784:
-				sendMessage('╰力量超越面具╯');break;
+				sendMessage('Powerful Transcendent Mask');break;
 			case 88785:
-				sendMessage('╰暴擊超越面具╯');break;
+				sendMessage('Keen Transcendent Mask');break;
 			case 88786:
-				sendMessage('╰暴威超越面具╯');break;
+				sendMessage('Bitter Transcendent Mask');break;
 			case 88787:
-				sendMessage('╰CD超越面具╯');break;
+				sendMessage('Energetic Transcendent Mask');break;
 			
 			case 88827:
-				sendMessage('╰腰带增幅剂I╯');break;
+				sendMessage('Belt Amplifier I');break;
 			case 88828:
-				sendMessage('╰项链增幅剂I╯');break;
+				sendMessage('Necklace Amplifier I');break;
 			case 88829:
-				sendMessage('╰耳环增幅剂I╯');break;
+				sendMessage('Earring Amplifier I');break;
 			case 88830:
-				sendMessage('╰戒指增幅剂I╯');break;
+				sendMessage('Ring Amplifier I');break;
 			case 88831:
-				sendMessage('╰饰环增幅剂I╯');break;
+				sendMessage('Circlet Amplifier I');break;
 			
 			case 88832:
-				sendMessage('╰腰带增幅剂II╯');break;
+				sendMessage('Belt Amplifier II');break;
 			case 88833:
-				sendMessage('╰项链增幅剂II╯');break;
+				sendMessage('Necklace Amplifier II');break;
 			case 88834:
-				sendMessage('╰耳环增幅剂II╯');break;
+				sendMessage('Earring Amplifier II');break;
 			case 88835:
-				sendMessage('╰戒指增幅剂II╯');break;
+				sendMessage('Ring Amplifier II');break;
 			case 88836:
-				sendMessage('╰饰环增幅剂II╯');break;
+				sendMessage('Circlet Amplifier II');break;
 			
 			case 96207:
-				sendMessage('合成水晶原石VII');break;
+				sendMessage('Dyad Niveot Structure');break;
 			
 			case 98281:
-				sendMessage('╰傳説刻印書箱子╯');break;
+				sendMessage('Superior Etching Box');break;
 			
 			// case 98500:
-				// sendMessage('╰阿勒堡雷亞金色結晶體╯');break;
+				// sendMessage('Golden Talent');break;
 			// case 98514:
-				// sendMessage('╰阿勒堡雷亞銀色結晶體╯');break;
+				// sendMessage('Silver Talent');break;
 			
-			case 98531:
-				sendMessage('╰冰冷裝備箱╯');break;	
-			case 98532:
-				sendMessage('╰呼嘯裝備箱╯');break;
+			case 88988:
+				sendMessage('Blightoath Handwear Chest');break;	
+			case 88991:
+				sendMessage('Blightoath Footwear Chest');break;
+				
+			case 88982:
+				sendMessage('Blightoath Weapon Chest');break;	
+			case 88985:
+				sendMessage('Blightoath Armor Chest');break;
+				
+            case 88996:
+				sendMessage('Celestial Treasure Chest');break;
+			case 88996:
+				sendMessage('Celestial Treasure Chest');break;
+				
+			case 88852:
+				sendMessage('Energetic Bahaar Mask');break;	
+			case 88851:
+				sendMessage('Bitter Bahaar Mask');break;	
+			case 88850:
+				sendMessage('Keen Bahaar Mask');break;	
+			case 88849:
+				sendMessage('Powerful Bahaar Mask');break;	
 			
 			default :
 				break;
@@ -155,7 +174,7 @@
 	})
 	
 	function sendMessage(msg) {
-		mod.command.message('发现 <font color="#00FFFF">' + msg + '</font>');
+		mod.command.message('Found <font color="#00FFFF">' + msg + '</font>');
 	}
 	
 }
